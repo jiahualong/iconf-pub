eb1ba7d33200328d0a62aa86733199e3ac87541f
JiaHualong <magic.hualong@gmail.com>
2017-10-06 16:57:56 +0800

    fix powerline style path

--- init.sh
+++ init.sh
@@ -27,8 +27,8 @@ ln -sf $ICONF_DIR/vim.conf/vimrc ~/.config/nvim/init.vim
 
 ## oh-my-zsh & themes
 git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
-git clone https://github.com/jeremyFreeAgent/oh-my-zsh-powerline-theme .oh-my-zsh-powerline-theme
-cp .oh-my-zsh-powerline-theme/powerline.zsh-theme .oh-my-zsh/themes/
+git clone https://github.com/jeremyFreeAgent/oh-my-zsh-powerline-theme ~/.oh-my-zsh-powerline-theme
+cp ~/.oh-my-zsh-powerline-theme/powerline.zsh-theme ~/.oh-my-zsh/themes/
 
 # clone vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
