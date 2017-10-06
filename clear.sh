dcf3e54678581a8d85d8911a9cbd7ada2ff30e70
JiaHualong <magic.hualong@gmail.com>
2017-10-06 16:55:17 +0800

    add clear.sh

---
+++ clear.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#############################################################
+#
+#	iconf-pub clear script	
+#	magic.hualong@gmail.com
+#
+#
+#############################################################
+
+rm -rf ~/.oh-my-zsh ~/.oh-my-zsh-powerline-theme ~/.tmux.conf ~/.vimrc ~/.zshrc
+echo 'clear finished'
